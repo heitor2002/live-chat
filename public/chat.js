54f71f4758b7f62c35491ca1093726162d8b5872
heitor2002 <montagnini.heitor@gmail.com>
2023-03-22 16:41:58 -0300

    feat: obtenção da mensagem.

--- public/chat.js
+++ public/chat.js
@@ -2,6 +2,27 @@ const socket = io();
 
 const urlSearch = new URLSearchParams(window.location.search);
 const username = urlSearch.get("nickname");
-const room = urlSearch.get("room")
+const room = urlSearch.get("room");
 
-console.log(username, room)
+socket.emit("room", {
+  username,
+  room,
+});
+
+document.getElementById("message_user").addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const message = e.target.value;
+
+    const data = {
+      room,
+      username,
+      message,
+    };
+
+    socket.on("message", data);
+
+    e.target.value = "";
+  }
+});
+
+console.log(username, room);
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,27 @@
 import { io } from "./http";
 
-io.on("connection", socket => {
-    console.log(socket.id)
-})
+interface RoomUser {
+  socket_id: string;
+  nickname: string;
+  room: string;
+}
+
+const users: RoomUser[] = [];
+
+io.on("connection", (socket) => {
+  socket.on("room", (data) => {
+    const userInRoom = users.find(
+      (user) => user.nickname === data.username && user.room === data.room
+    );
+
+    userInRoom
+      ? (userInRoom.socket_id = socket.id)
+      : users.push({
+          room: data.room,
+          nickname: data.username,
+          socket_id: socket.id,
+        });
+
+        console.log(users)
+  });
+});
