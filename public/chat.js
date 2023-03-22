da752e1babd40cfaa30a527d4672140322dbc0b8
heitor2002 <montagnini.heitor@gmail.com>
2023-03-22 16:02:48 -0300

    feat: obtendo dados pela url.

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,7 @@
-const socket = io()
+const socket = io();
+
+const urlSearch = new URLSearchParams(window.location.search);
+const username = urlSearch.get("nickname");
+const room = urlSearch.get("room")
+
+console.log(username, room)
--- src/http.ts
+++ src/http.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import http from "http";
+import path from "path";
 import {Server} from "socket.io";
 
 const port = 3000;
 
 const app = express();
+app.use(express.static(path.join(__dirname, "..", "public")))
 const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp)
